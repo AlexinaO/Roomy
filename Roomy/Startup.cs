13b141b6e4fa2df45303c20c2c7f17e0ef71abbd
Vincent <vleclerc@inow.fr>
2018-06-11 17:07:26 +0200

    add mvc services and routes

--- Roomy/Startup.cs
+++ Roomy/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -28,6 +29,8 @@ namespace Roomy
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,10 +64,20 @@ namespace Roomy
                 }
             });
 
-            app.Run(async (context) =>
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(ConfigureRoute);
+
+            /*app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello world!");
-            });
+            });*/
+        }
+
+        private void ConfigureRoute(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute(
+                name: "Default",
+                template: "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
