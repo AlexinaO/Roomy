a5fb73fec3eb0bea2975d1f16cf96e32fda735a1
Vincent <vleclerc@inow.fr>
2018-06-12 10:35:32 +0200

    add taghelpers

---
+++ Roomy/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roomy.Models
+{
+    public class User
+    {
+        public string LastName { get; set; }
+
+        public string Firstname { get; set; }
+
+       
+    }
+}
